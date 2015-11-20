ecd89a965b8e0a6920f11ffc136ee19b0aafea80
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2015-11-20 19:41:29 +0800

    Added Main file jg-force-full-width-shortcode.php and Added in plugin's name and information as php comments

---
+++ jg-force-full-width-shortcode.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Force Full Width Shortcode
+ * Plugin URI: http://jacobgoh.com
+ * Description: This plugin's shortcode allows you to create full screen width element in any page or post.
+ * Version: 1.0.0
+ * Author: Jacob Goh
+ * Author URI: http://jacobgoh.com
+ * License: GPL2
+ */
